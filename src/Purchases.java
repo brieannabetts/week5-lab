dac26c02a76a60f032806d623786fbcf5bb37205
brieannabetts <bgbetts@lakeheadu.ca>
2021-02-23 10:32:50 -0600

    Purchases class complete

--- src/Purchases.java
+++ src/Purchases.java
@@ -1,8 +1,47 @@
 import java.util.ArrayList;
 public class Purchases {
     private ArrayList<GroceryItem> purchases;
-
     public Purchases() {
         this.purchases = new ArrayList<>();
     }
+    public void addGroceryItem(GroceryItem item)
+    {
+        purchases.add(item);
+    }
+    public double getTotalPrice()
+    {
+        double total = 0;
+        for (GroceryItem groceryItem : purchases)
+        {
+            total += groceryItem.getPrice();
+        }
+        return total;
+    }
+
+    public String getGroceryNames()
+    {
+        String groceries = "";
+        for (GroceryItem groceryItem:purchases)
+        {
+            groceries += groceryItem.getName() + ", ";
+        }
+        return groceries.substring(0,groceries.length()-2);
+    }
+
+    public String getCategories()
+    {
+        String groceries = "";
+        for (GroceryItem groceryItem:purchases)
+        {
+            groceries += groceryItem.getCategory() + ", ";
+        }
+        return groceries.substring(0,groceries.length()-2);
+    }
+
+    public String toString()
+    {
+        return String.format("The grocery list has %d items with a total cost of $%.2f",
+                purchases.size(), getTotalPrice());
+    }
+
 }
